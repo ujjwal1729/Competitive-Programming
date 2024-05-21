f61863ebbdab6646d612a525c1ebe881fe840f1a
ujjwal1729 <126157177+ujjwal1729@users.noreply.github.com>
2024-05-21 23:52:10 +0530

    Update dfs & no-of-components.cpp

--- Graph/dfs & no-of-components.cpp
+++ Graph/dfs & no-of-components.cpp
@@ -1,3 +1,6 @@
+//doing dfs and counting total number of components in a graph.
+// also assigning the numbers to different components.
+
 vector<vector<ll>>edges;
 vector<bool>visited;
 vector<ll>component;
@@ -10,3 +13,29 @@ void dfs(ll node,ll parent,ll count){
         if(i != parent) dfs(i,node,count);
     }
 }
+ 
+int main(){
+    ll n,e;
+    cin >> n >> e;
+    edges.assign(n+1,vector<ll>());
+    component.assign(n+1,0);
+    fr(1,e+1,i){
+        ll a,b;
+        cin >> a >> b;
+        edges[a].pb(b);
+        edges[b].pb(a);
+    }
+    visited.assign(n+1,false);
+    ll count=0;
+    ll prev;
+    vector<pair<ll,ll>>ans;
+    fr(1,n+1,i){
+        if(visited[i] == false) {
+            count++;
+            dfs(i,0,count);
+            if(i!=1){
+                ans.pb({i,prev});
+            }
+            prev=i;
+        }
+    }   
