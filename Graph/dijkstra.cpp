67a543def2007a35ec537db48f09c64756ddadf1
ujjwal1729 <126157177+ujjwal1729@users.noreply.github.com>
2024-05-22 12:20:33 +0530

    Create dijkstra.cpp

---
+++ Graph/dijkstra.cpp
@@ -0,0 +1,30 @@
+//shortest path, single source
+// cses : shortest routes1
+
+ll n,e;
+vector<vector<pair<ll,ll>>>edges;
+vector<ll>dist;
+vector<ll>parent;
+
+void dijkstra(int s){
+    parent.assign(n+1,-1);
+    dist.assign(n+1,INF);
+    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> q;
+    q.push({0, s});
+    dist[s] = 0;
+    while (!q.empty()) {
+        ll d_v = q.top().first;
+        ll v = q.top().second;
+        q.pop();
+        if (d_v != dist[v]) continue;
+        for (auto edge : edges[v]) {
+            int len = edge.second;
+            int to = edge.first;
+            if ((dist[v] + len) < dist[to]) {
+                dist[to] = dist[v] + len;
+                parent[to] = v;
+                q.push({dist[to], to});
+            }
+        }
+    }
+}
