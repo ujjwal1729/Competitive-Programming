5f3f700271fa29aa26a387ec56f8a99cdf3d19f4
ujjwal1729 <126157177+ujjwal1729@users.noreply.github.com>
2024-08-13 14:59:41 +0530

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -21,10 +21,10 @@ ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} retur
 ll lcm(ll a,ll b) {return a*b/gcd(a,b);}
 ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
 ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return ((a - b+m) % m);}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll mod_add(ll a, ll b, ll m) {return (a + b) % m;}
+ll mod_mul(ll a, ll b, ll m) {return (a * b)% m;}
+ll mod_sub(ll a, ll b, ll m) {return ((a - b+m) % m);}
+ll mod_div(ll a, ll b, ll m) {return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
 
 //...................................oredered set........................................
 
